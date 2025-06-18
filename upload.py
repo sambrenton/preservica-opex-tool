792b19ebbd19a6ea8451fe000faeb2ddfec397b2
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2025-06-18 10:46:32 +0100

    Add framework for ingest and link function
    Add framework for ingest and link function - currently without directory name input into function as not saved in variable

--- upload.py
+++ upload.py
@@ -4,10 +4,11 @@ import argparse
 import datetime
 import sys
 import configparser
+from pyPreservica import *
 
 parser = argparse.ArgumentParser(prog='upload',
                                  description='Tool to upload files to preservica')
-parser.add_argument('--config', help='Config file (default ~/S3.ini)', default='~/S3.ini')
+parser.add_argument('--auth', help='Credentials/authentication file (default ~/S3.ini)', default='~/S3.ini')
 parser.add_argument('-t', '--target', required=True, help='The folder containing the opex files')
 parser.add_argument('-b', '--bucket', required=True, help="Bucket (defined in config file)")
 parser.add_argument('-c', '--container', required=True, help="Container to upload to")
@@ -15,6 +16,7 @@ parser.add_argument('-v', '--verbose', action='store_true',
                     help='Explain what is happening')
 parser.add_argument('-d', '--dry-run', action='store_true',
                     help="Don't actually perform any actions, for testing")
+parser.add_argument('-i' '--ingest', action='store_true', help='Optional - run ingest and link workflow in Preservica after upload')
 
 sys.argv.pop(0)  # why do I need this?
 args = parser.parse_args(sys.argv)
@@ -27,12 +29,15 @@ if not args.bucket in config:
     print(f"No bucket named {args.bucket} in config file {args.config}")
     sys.exit(1)
 
-# get correct credentials for required bucket
+# get correct credentials for required bucket and Preservica API
 ACCESS_KEY = config[args.bucket]['ACCESS_KEY']
 SECRET_KEY = config[args.bucket]['SECRET_KEY']
 BUCKET_NAME = config[args.bucket]['BUCKET_NAME']
 OPEX_DIR = args.target
 UPLOAD_BASE = os.path.basename(OPEX_DIR)
+USERNAME = config['username']
+PASSWORD = config['password']
+SERVER = config['server']
 
 def sort_key(upload_entry):
     # Take the target path, e.g. /foo/bar/filename
@@ -60,6 +65,22 @@ def map_upload(dest, container, dir):
 
     return f"{container}/{dir}{dest}"
 
+def ingest_and_link(dir_in_bucket:str): # TODO access correct directory path via variable 
+      
+    """
+    Trigger Ingest and Link Theatre workflow in Preservica following upload
+    Will run ingest and link TC workflow using workflow ID
+    OpexContainerDirectory needs full bucket path eg. "opex_tc/working-2025-05-21T1006"
+    """
+
+    client = WorkflowAPI(username=USERNAME, password=PASSWORD, server=SERVER)
+
+    workflow_context = next(wf for wf in client.get_workflow_contexts_by_type("Ingest") if wf.workflow_id == '42')
+    # TODO - Have options for multiple ingest workflows - eg. TC/SC, linked/unlinked etc 
+
+    client.start_workflow_instance(workflow_context, OpexContainerDirectory=dir_in_bucket)
+    # TODO use correlation ID returned from 'start_workflow_instance' to tracking/monitoring/callback info
+    
 upload_plan = load_uploads(OPEX_DIR, 'to_upload.txt')
 
 # We will use this to allow repeated uploads of the same material
@@ -77,7 +98,11 @@ s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_k
 
 for source, target in timestamped_upload_plan:
     print(f"Upload {source}\n\tto {BUCKET_NAME}\n\tas {target}")
-    if not args.dry_run:
+    if not args.dry_run: # TODO - is this a typo ?? parser config has 'dry-run' not 'dry_run'
         s3_client.upload_file(source, BUCKET_NAME, target)
 
 print(f"\nFinished. See {upload_dir} in {BUCKET_NAME}/{args.container}")
+
+if args.ingest:
+    print(f"\n Starting Ingest Workflow")
+    ingest_and_link()
